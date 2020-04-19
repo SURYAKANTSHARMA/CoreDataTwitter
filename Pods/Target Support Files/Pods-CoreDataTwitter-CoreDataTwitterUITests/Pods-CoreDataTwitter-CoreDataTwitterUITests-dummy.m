bbcab49cd047e1b63502156e2f68bf61912c8cde
Suryakant Sharma <suryakantsharma84@gmail.com>
2020-04-19 13:46:36 +0530

    Add pod file

---
+++ Pods/Target Support Files/Pods-CoreDataTwitter-CoreDataTwitterUITests/Pods-CoreDataTwitter-CoreDataTwitterUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CoreDataTwitter_CoreDataTwitterUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CoreDataTwitter_CoreDataTwitterUITests
+@end
---
+++ Pods/Target Support Files/Pods-CoreDataTwitter/Pods-CoreDataTwitter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CoreDataTwitter : NSObject
+@end
+@implementation PodsDummy_Pods_CoreDataTwitter
+@end
